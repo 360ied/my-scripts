9d7debc3067f5a1400d0c7d9734fe6152d388251
360ied <19516527+360ied@users.noreply.github.com>
2022-05-09 20:08:19 -0400

    parallelize packscreenshots.sh

--- packscreenshots.sh
+++ packscreenshots.sh
@@ -3,10 +3,12 @@
 set -eoux pipefail
 
 fol='.tmppackscreenshots'
+packname="$(date -Idate)-screenshots.tar"
 
 rm -rf "$fol"
 mkdir -p "$fol"
-cp --reflink=always -t "$fol" ./*_*_grim.png
-#optimizepng.fish $fol/*
-find "$fol" -type f -print0 | xargs -0P "$(nproc)" optimizepng.fish
-tar cf "$fol/$(date -Idate).tar" ./*_*_grim_pngquant.png
+find "." -type f -print0 | xargs -0 cp --reflink=always -t "$fol"
+find "$fol" -type f -print0 | parallel -0 optimizepng.fish
+printf "%s\0" $fol/*_*_grim_pngquant.png | tar cf "$fol/$packname" --strip-components=1 --null -T /dev/stdin
+mv "$fol/$packname" "./$packname"
+rm -rf "$fol"
