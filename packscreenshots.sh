d77a37b8632c68a602f6f1bc4511eac45929a579
360ied <19516527+360ied@users.noreply.github.com>
2022-05-09 20:23:10 -0400

    packscreenshots.sh: fix tar paths and pngquant errors

--- packscreenshots.sh
+++ packscreenshots.sh
@@ -7,8 +7,10 @@ packname="$(date -Idate)-screenshots.tar"
 
 rm -rf "$fol"
 mkdir -p "$fol"
-find "." -type f -print0 | xargs -0 cp --reflink=always -t "$fol"
+printf "%s\0" ./*_*_grim.png | xargs -0 cp --reflink=always -t "$fol"
 find "$fol" -type f -print0 | parallel -0 optimizepng.fish
-printf "%s\0" $fol/*_*_grim_pngquant.png | tar cf "$fol/$packname" --strip-components=1 --null -T /dev/stdin
+cd "$fol"
+printf "%s\0" ./*_*_grim_pngquant.png | tar cf "$packname" --strip-components=1 --null -T /dev/stdin
+cd -
 mv "$fol/$packname" "./$packname"
 rm -rf "$fol"
